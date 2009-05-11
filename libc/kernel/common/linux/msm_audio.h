f378cfd6ed1b6ea87d0cee166bd3788df77ef9a3
Zeng, Xiaoyun <xiaoyunz@quicinc.com>
2009-05-11 16:22:19 -0700

    Bionic: Updating msm_audio.h to enable AGC/AVC
    This corresponds to kernel gerrit ID: 320

--- libc/kernel/common/linux/msm_audio.h
+++ libc/kernel/common/linux/msm_audio.h
@@ -118,6 +118,9 @@ struct msm_snd_endpoint {
 
 #define SND_GET_ENDPOINT _IOWR(SND_IOCTL_MAGIC, 5, struct msm_snd_endpoint *)
 
+#define SND_AVC_CTL _IOW(SND_IOCTL_MAGIC, 6, unsigned *)
+#define SND_AGC_CTL _IOW(SND_IOCTL_MAGIC, 7, unsigned *)
+
 struct msm_audio_pcm_config {
  uint32_t pcm_feedback;
  uint32_t buffer_count;
