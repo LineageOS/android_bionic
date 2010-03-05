e88cc3d8cb2989f66624d018a6f5fa559c51460b
Gary King <gking@nvidia.com>
2010-03-04 16:06:41 -0800

    [bionic] implement work around for tegra errata 657451
    tegra 2 processors have a bug in the register read path of bit 20 of
    the CP15 c13, 3 register (used for software thread local storage)
    
    the kernel work-around for this bug is to mux the value from bit 20
    into bit 0; since the TLS value used by Android is an aligned address,
    bit 0 is known to be available.
    
    Change-Id: I989b8f7a1189094f0c1a6ba9ff2a2dcba1f6c796
    Reviewed-on: http://git-master/r/773
    Reviewed-by: Gary King <gking@nvidia.com>
    Tested-by: Gary King <gking@nvidia.com>

--- libc/private/bionic_tls.h
+++ libc/private/bionic_tls.h
@@ -93,10 +93,17 @@ extern int __set_tls(void *ptr);
  * Note that HAVE_ARM_TLS_REGISTER is build-specific
  * (it must match your kernel configuration)
  */
+#  ifdef HAVE_TEGRA_ERRATA_657451
+#    define __munge_tls(_v) ( ((_v)&~((1ul<<20)|1ul)) | (((_v)&0x1)<<20) )
+#  else
+#    define __munge_tls(_v) (_v)
+#endif
+
 #  ifdef HAVE_ARM_TLS_REGISTER
 #    define __get_tls() \
     ({ register unsigned int __val asm("r0"); \
        asm ("mrc p15, 0, r0, c13, c0, 3" : "=r"(__val) ); \
+       __val = __munge_tls(__val); \
        (volatile void*)__val; })
 #  else /* !HAVE_ARM_TLS_REGISTER */
 #    define __get_tls() ( *((volatile void **) 0xffff0ff0) )
