e12632cb3fe8aa2eeea75743c968a9cb5cc9ba5c
Jignesh Mehta <jigneshm@codeaurora.org>
2010-07-21 14:13:38 -0700

    camera: Add VPE driver support
    Change-Id: If1a43f878a0fec143ee3d8d8b2c6032999a31bbb

--- libc/kernel/common/media/msm_camera.h
+++ libc/kernel/common/media/msm_camera.h
@@ -18,7 +18,11 @@
 #include <linux/types.h>
 #include <asm/sizes.h>
 #include <linux/ioctl.h>
+#ifdef MSM_CAMERA_GCC
+#include <time.h>
+#else
 #include <linux/time.h>
+#endif
 
 #define MSM_CAM_IOCTL_MAGIC 'm'
 
@@ -64,6 +68,10 @@
 
 #define MSM_CAM_IOCTL_SENSOR_IO_CFG   _IOW(MSM_CAM_IOCTL_MAGIC, 21, struct sensor_cfg_data *)
 
+#define MSM_CAM_IOCTL_CONFIG_VPE   _IOW(MSM_CAM_IOCTL_MAGIC, 27, struct msm_camera_vpe_cfg_cmd *)
+
+#define MSM_CAM_IOCTL_AXI_VPE_CONFIG   _IOW(MSM_CAM_IOCTL_MAGIC, 28, struct msm_camera_vpe_cfg_cmd *)
+
 #define MSM_CAMERA_LED_OFF 0
 #define MSM_CAMERA_LED_LOW 1
 #define MSM_CAMERA_LED_HIGH 2
@@ -101,6 +109,15 @@ struct msm_vfe_evt_msg {
  unsigned short type;
  unsigned short msg_id;
  unsigned int len;
+ uint32_t frame_id;
+ void *data;
+};
+
+struct msm_vpe_evt_msg {
+ unsigned short type;
+ unsigned short msg_id;
+ unsigned int len;
+ uint32_t frame_id;
  void *data;
 };
 
@@ -174,8 +191,16 @@ struct msm_camera_cfg_cmd {
 #define CMD_STATS_IHIST_ENABLE 38
 #define CMD_STATS_RS_ENABLE 39
 #define CMD_STATS_CS_ENABLE 40
+#define CMD_VPE 41
+#define CMD_AXI_CFG_VPE 42
 
 struct msm_vfe_cfg_cmd {
+ int cmd_type;
+ uint16_t length;
+ void *value;
+};
+
+struct msm_vpe_cfg_cmd {
  int cmd_type;
  uint16_t length;
  void *value;
@@ -202,7 +227,8 @@ struct camera_enable_cmd {
 #define MSM_PMEM_SKIN 13
 #define MSM_PMEM_VIDEO 14
 #define MSM_PMEM_PREVIEW 15
-#define MSM_PMEM_MAX 16
+#define MSM_PMEM_VIDEO_VPE 16
+#define MSM_PMEM_MAX 17
 
 #define STAT_AEAW 0
 #define STAT_AEC 1
