70be26a5ef309ba57da148be01da3592ac1c34d1
Jim Huang <jserv@0xlab.org>
2011-12-20 15:53:56 +0800

    Add ARMv7 optimized strlen()
    Merge the ARM optimized strlen() routine from Linaro.  Although it is
    optimized for ARM Cortex-A9, the performance is still reasonably faster
    than the original on Cortex-A8 machines.
    
    Reference benchmark on Nexus S (ARM Cortex-A8; 1 GHz):
    
    [before]
                 prc thr   usecs/call      samples   errors cnt/samp     size
    strlen_1k      1   1      1.31712           97        0     1000     1024
    
    [after]
                 prc thr   usecs/call      samples   errors cnt/samp     size
    strlen_1k      1   1      1.05855           96        0     1000     1024
    
    Change-Id: I809928804726620f399510af1cd1c852ed754403

--- libc/Android.mk
+++ libc/Android.mk
@@ -352,7 +352,6 @@ libc_common_src_files += \
 	arch-arm/bionic/memset.S \
 	arch-arm/bionic/setjmp.S \
 	arch-arm/bionic/sigsetjmp.S \
-	arch-arm/bionic/strlen.c.arm \
 	arch-arm/bionic/strcpy.S \
 	arch-arm/bionic/strcmp.S \
 	arch-arm/bionic/syscall.S \
@@ -360,6 +359,11 @@ libc_common_src_files += \
 	string/bcopy.c \
 	string/strncmp.c \
 	unistd/socketcalls.c
+ifeq ($(ARCH_ARM_HAVE_ARMV7A),false)
+libc_common_src_files += arch-arm/bionic/strlen-armv7.S
+else
+libc_common_src_files += arch-arm/bionic/strlen.c.arm
+endif
 
 
 
