e5b564f68a3238e82c2d97df3a4544eff00a99bf
Prachee Ramsinghani <pracheer@codeaurora.org>
2012-07-26 16:20:22 +0530

    msm7627a: Enable neon optimized memove and pow functions.
    Define SPARROW_NEON_OPTIMIZATION flag so that neon optimized
    memove and pow functions are used. Also add Corresponding
    definitions in make files.
    
    Change-Id: I12089fc7002e3ec294e63632bd84e395fbd24936

--- libm/src/e_pow.c
+++ libm/src/e_pow.c
@@ -13,7 +13,7 @@
 static char rcsid[] = "$FreeBSD: src/lib/msun/src/e_pow.c,v 1.11 2005/02/04 18:26:06 das Exp $";
 #endif
 
-#if defined(KRAIT_NEON_OPTIMIZATION)
+#if defined(KRAIT_NEON_OPTIMIZATION) || defined(SPARROW_NEON_OPTIMIZATION)
 double pow_neon(double x, double y);
 #endif
 
@@ -205,7 +205,7 @@ __ieee754_pow(double x, double y)
 	    t1 = u+v;
 	    SET_LOW_WORD(t1,0);
 	    t2 = v-(t1-u);
-#if defined(KRAIT_NEON_OPTIMIZATION)
+#if defined(KRAIT_NEON_OPTIMIZATION) || defined(SPARROW_NEON_OPTIMIZATION)
 	} else if (ix <= 0x40100000 && iy <= 0x40100000 && hy > 0 && hx > 0) {
 		return pow_neon(x,y);
 #endif
