9b2c88495d0bd7171a0b84d3594ea80d4e97247c
Steve Kondik <shade@chemlab.org>
2012-11-18 17:44:49 -0800

    Revert "memmove: Fix the abuse of memcpy() for overlapping regions"
    This reverts commit af820b84625a14eaf199c82c6b4a8773b145668f.

--- libc/string/memmove.c
+++ libc/string/memmove.c
@@ -32,10 +32,10 @@ void *memmove(void *dst, const void *src, size_t n)
 {
   const char *p = src;
   char *q = dst;
-  /* We can use the optimized memcpy if the destination is completely below the
-   * source (i.e. q+n <= p), or if it is completely over it (i.e. q >= p+n).
+  /* We can use the optimized memcpy if the destination is below the
+   * source (i.e. q < p), or if it is completely over it (i.e. q >= p+n).
    */
-  if (__builtin_expect((q + n < p) || (q >= p + n), 1)) {
+  if (__builtin_expect((q < p) || ((size_t)(q - p) >= n), 1)) {
     return memcpy(dst, src, n);
   } else {
     bcopy(src, dst, n);
