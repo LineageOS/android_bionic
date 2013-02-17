3ded0b3b70243b7e709bfe54cbb3471fdf1c6a8b
Steve Kondik <shade@chemlab.org>
2013-02-17 06:15:56 -0800

    libc: Remove Scorpion memmove() routine
     * This code is slower than the new implementation under every
       circumstance on Scorpion devices. Remove it and standardize on one
       implementation.
     * Also remove "optimized" memmove_words, which was just a pointer to
       memmove. The performance was identical to the C version on Krait, and
       has regressions on other platforms. This function is only used for
       System.arraycopy in Dalvik.
    
    Change-Id: I42d06fbd7c56d9874b80a0e768dbadf0fa5025a4

--- libc/Android.mk
+++ libc/Android.mk
@@ -278,6 +278,7 @@ libc_common_src_files := \
 	bionic/libc_init_common.c \
 	bionic/logd_write.c \
 	bionic/md5.c \
+	bionic/memmove_words.c \
 	bionic/pututline.c \
 	bionic/realpath.c \
 	bionic/sched_getaffinity.c \
@@ -387,6 +388,7 @@ libc_common_src_files += \
 	string/strncmp.c \
 	unistd/socketcalls.c
 
+# String routines optimized for ARMv7
 ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
 libc_common_src_files += arch-arm/bionic/memchr.S
 libc_common_src_files += arch-arm/bionic/strlen-armv7.S
@@ -404,21 +406,14 @@ endif
 
 # Check if we want a neonized version of memmove instead of the
 # current ARM version
-ifeq ($(TARGET_USE_SCORPION_BIONIC_OPTIMIZATION),true)
-libc_common_src_files += \
-	arch-arm/bionic/memmove.S \
-	bionic/memmove_words.c
-else
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
  libc_common_src_files += \
 	arch-arm/bionic/memmove.S
  else # Other ARM
  libc_common_src_files += \
 	string/bcopy.c \
-	string/memmove.c.arm \
-	bionic/memmove_words.c
- endif # !TARGET_USE_KRAIT_BIONIC_OPTIMIZATION
-endif # !TARGET_USE_SCORPION_BIONIC_OPTIMIZATION
+	string/memmove.c.arm
+endif # ARCH_ARM_HAVE_NEON
 
 # If the kernel supports kernel user helpers for gettimeofday, use
 # that instead.
