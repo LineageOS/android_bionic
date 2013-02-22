a3aabed028141033e0accd875bbab0bf8f15b65c
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2013-02-22 00:56:53 -0300

    libc: Replaced O(n^2) strstr with O(n)
    Took it from glibc.
    
    Change-Id: Ide8866a8db81c3df99ebae65a0c35fe1a34679ce

--- libc/string/strstr.c
+++ libc/string/strstr.c
@@ -1,56 +1,117 @@
-/*	$OpenBSD: strstr.c,v 1.5 2005/08/08 08:05:37 espie Exp $ */
-/*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+/* Copyright (C) 1994,1996,1997,2000,2001,2003 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+/*
+ * My personal strstr() implementation that beats most other algorithms.
+ * Until someone tells me otherwise, I assume that this is the
+ * fastest implementation of strstr() in C.
+ * I deliberately chose not to comment it.  You should have at least
+ * as much fun trying to understand it, as I had to write it :-).
  *
- * This code is derived from software contributed to Berkeley by
- * Chris Torek.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
+ * Stephen R. van den Berg, berg@pool.informatik.rwth-aachen.de */
 
 #include <string.h>
 
-/*
- * Find the first occurrence of find in s.
- */
-char *
-strstr(const char *s, const char *find)
-{
-	char c, sc;
-	size_t len;
+typedef unsigned chartype;
 
-	if ((c = *find++) != 0) {
-		len = strlen(find);
-		do {
-			do {
-				if ((sc = *s++) == 0)
-					return (NULL);
-			} while (sc != c);
-		} while (strncmp(s, find, len) != 0);
-		s--;
-	}
-	return ((char *)s);
+/*
+ * Find the first occurrence of pneedle in phaystack
+ */
+
+char *
+strstr (phaystack, pneedle)
+     const char *phaystack;
+     const char *pneedle;
+{
+  const unsigned char *haystack, *needle;
+  chartype b;
+  const unsigned char *rneedle;
+
+  haystack = (const unsigned char *) phaystack;
+
+  if ((b = *(needle = (const unsigned char *) pneedle)))
+    {
+      chartype c;
+      haystack--;               /* possible ANSI violation */
+
+      {
+        chartype a;
+        do
+          if (!(a = *++haystack))
+            goto ret0;
+        while (a != b);
+      }
+
+      if (!(c = *++needle))
+        goto foundneedle;
+      ++needle;
+      goto jin;
+
+      for (;;)
+        {
+          {
+            chartype a;
+            if (0)
+            jin:{
+                if ((a = *++haystack) == c)
+                  goto crest;
+              }
+            else
+              a = *++haystack;
+            do
+              {
+                for (; a != b; a = *++haystack)
+                  {
+                    if (!a)
+                      goto ret0;
+                    if ((a = *++haystack) == b)
+                      break;
+                    if (!a)
+                      goto ret0;
+                  }
+              }
+            while ((a = *++haystack) != c);
+          }
+        crest:
+          {
+            chartype a;
+            {
+              const unsigned char *rhaystack;
+              if (*(rhaystack = haystack-- + 1) == (a = *(rneedle = needle)))
+                do
+                  {
+                    if (!a)
+                      goto foundneedle;
+                    if (*++rhaystack != (a = *++needle))
+                      break;
+                    if (!a)
+                      goto foundneedle;
+                  }
+                while (*++rhaystack == (a = *++needle));
+              needle = rneedle; /* took the register-poor aproach */
+            }
+            if (!a)
+              break;
+          }
+        }
+    }
+foundneedle:
+  return (char *) haystack;
+ret0:
+  return 0;
 }
