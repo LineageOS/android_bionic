456c0e8cbe4ed2da25317e8e46ed327a14fbeb1c
Will Newton <will.newton@linaro.org>
2013-07-01 11:15:27 +0100

    Bionic: Add AOSP master cortex optimizations
    libc/arch-arm/bionic/memcpy.a9.S: memcpy from cortex-strings.
    
    This memcpy code uses NEON/VFP to achieve very good performance
    on ARMv7-A processors. It is specifically tuned for A15 but should
    provide good performance on A9 also. It is equivalent to the code
    in cortex-strings rev 116.
    
    This patch is a follow up the existing gerrit change:
    
    I7f6f77995f3ca903ad9c66d14261441667a2a935
    
    But this version includes a tweak for performance on misaligned
    buffers.
    
    Change-Id: I285abac0068f8ae29a1cbf7862ea8590aadaf0a7
    Signed-off-by: Will Newton <will.newton@linaro.org>
    
    libc/arch-arm/bionic/memcpy.a9.S: memcpy from cortex-strings.
    
    This memcpy code uses NEON/VFP to achieve very good performance
    on ARMv7-A processors. It is specifically tuned for A15 but should
    provide good performance on A9 also. It is equivalent to the code
    in cortex-strings rev 116.
    
    This patch is a follow up the existing gerrit change:
    
    I7f6f77995f3ca903ad9c66d14261441667a2a935
    
    This version includes a tweak for performance on misaligned
    buffers and splits the header comment into license and
    documentation sections.
    
    Change-Id: Ibd2e23c8d8e01357ba0247be1d05192de3ceba69
    Signed-off-by: Will Newton <will.newton@linaro.org>
    
    Add new optimized strlen for arm.
    
    This optimized version is primarily targeted at cortex-a15.
    
    Tested on all nexus devices using the system/extras/libc_test strlen test.
    Tested alignments from 1 to 32 that are powers of 2.
    Tested that strlen does not cross page boundaries at all alignments.
    
    Speed improvements listed below:
    
    cortex-a15
    - Sizes >= 32 bytes, ~75% improvement.
    - Sizes >= 1024 bytes, ~250% improvement.
    
    cortex-a9
    - Sizes >= 32 bytes, ~75% improvement.
    - Sizes >= 1024 bytes, ~85% improvement.
    
    krait
    - Sizes >= 32 bytes, ~95% improvement.
    - Sizes >= 1024 bytes, ~160% improvement.
    
    Merge from internal master.
    
    (cherry-picked from 2fc071797743b88a9a47427d46baed7c7b24f4d2)
    
    Change-Id: I1ceceb4e745fd68e9d946f96d1d42e0cdaff6ccf
    
    Conflicts:
    	libc/arch-arm/generic/generic.mk
    	libc/arch-arm/krait/krait.mk
    
    libgcc_compat: Introduce __aeabi_lasr for cortex-a9 and higher
    
    This is needed when passing -mcpu=cortex-a9 or higher on a modern
    toolchain for prebuilt library compatibility
    
    Change-Id: I73eb2393377914ae26216a8c2828ad973d1c1225

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -2,5 +2,7 @@ $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/krait/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/krait/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
 $(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
+# Use cortex-a15 version of strlen.
+$(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a15/bionic/strlen.S)
 
 #include bionic/libc/arch-arm/generic/generic.mk
