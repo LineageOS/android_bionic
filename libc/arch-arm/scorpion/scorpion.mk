ffe37afb20127e1c445bb8a599c5d1519e4953ef
Steve Kondik <shade@chemlab.org>
2013-08-09 21:56:10 -0700

    libc: Add Scorpion-optimized memcpy and memset
     * Optimized routines from Qualcomm for msm8660 class SoCs
     * Enable with TARGET_CPU_VARIANT := scorpion
     * Use cortex-a15 memcpy and krait memset routines
    
    Change-Id: I01d0f22efba5a418ddd20fca0d0c570d855e0f6f

---
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -0,0 +1,15 @@
+# Use krait versions of memset/strcmp/memmove
+libc_bionic_src_files_arm += \
+    arch-arm/krait/bionic/memset.S \
+    arch-arm/krait/bionic/strcmp.S \
+    arch-arm/krait/bionic/memmove.S
+
+# Use cortex-a15 versions of memcpy/strcat/strcpy/strlen
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a15/bionic/memchr.S \
+    arch-arm/cortex-a15/bionic/memcpy.S \
+    arch-arm/cortex-a15/bionic/strcat.S \
+    arch-arm/cortex-a15/bionic/strcpy.S \
+    arch-arm/cortex-a15/bionic/strlen.S \
+    arch-arm/cortex-a15/bionic/__strcat_chk.S \
+    arch-arm/cortex-a15/bionic/__strcpy_chk.S \
