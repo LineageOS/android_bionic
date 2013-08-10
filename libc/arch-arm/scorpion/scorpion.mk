704ba2246c77789bde233dac59bb87f3d7c1e497
Steve Kondik <shade@chemlab.org>
2013-08-09 21:56:10 -0700

    libc: Add Scorpion-optimized memcpy and memset
     * Optimized routines from Qualcomm for msm8660 class SoCs
     * Enable with TARGET_CPU_VARIANT := scorpion
    
    Change-Id: I01d0f22efba5a418ddd20fca0d0c570d855e0f6f

---
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -0,0 +1,6 @@
+$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/scorpion/bionic/memcpy.S)
+$(call libc-add-cpu-variant-src,MEMSET,arch-arm/scorpion/bionic/memset.S)
+$(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
+$(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
+
+#include bionic/libc/arch-arm/generic/generic.mk
