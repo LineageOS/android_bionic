9da0db97dd4960ca6e0fbb0e6e96179bda43f3fb
Steve Kondik <shade@chemlab.org>
2013-08-10 20:14:06 -0700

    libc: Use the fast strlen on Scorpion too
    Change-Id: I9ba50a469de27269adc008c2a96767830ad6b70b

--- libc/arch-arm/scorpion/scorpion.mk
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -2,5 +2,6 @@ $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/scorpion/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/scorpion/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
 $(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
+$(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a15/bionic/strlen.S)
 
 #include bionic/libc/arch-arm/generic/generic.mk
