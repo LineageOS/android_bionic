1bfaf614869c6d17c6a074f399a7ef5a54521bcc
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2013-08-12 16:44:52 +0200

    tegra2: add missing strcpy and strcat
    These applets are no more common since recent optimisations
    
    strcpy from cortex-a9
    strcat from cm10.1
    
    Change-Id: Icfedf17aa4f9fa30e7f63b19bd6d4ac533e5e0e9

--- libc/arch-arm/tegra2/tegra2.mk
+++ libc/arch-arm/tegra2/tegra2.mk
@@ -1,10 +1,12 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/tegra2/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/tegra2/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/tegra2/bionic/strcmp.S)
+$(call libc-add-cpu-variant-src,STRCPY,arch-arm/tegra2/bionic/strcpy.S)
 $(call libc-add-cpu-variant-src,MEMMOVE,bionic/memmove.c.arm)
 $(call libc-add-cpu-variant-src,BCOPY,string/bcopy.c.arm)
 
 # armv7-a (non neon) ones from cm10.1
+$(call libc-add-cpu-variant-src,STRCAT,arch-arm/tegra2/bionic/strcat.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/tegra2/bionic/strlen.S)
 $(call libc-add-cpu-variant-src,MEMCHR,arch-arm/tegra2/bionic/memchr.S)
 
