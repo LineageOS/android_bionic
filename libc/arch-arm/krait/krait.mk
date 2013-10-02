2d9285e5c9b2aa94fa6d2c2b24756230ca72bdaa
Brent DeGraaf <bdegraaf@codeaurora.org>
2013-10-02 09:47:11 -0400

    Performance: krait: Re-factor high-performance memcpy for thumb2
    The majority of libc under bionic is built for thumb2.  Refactor
    the high performance memcpy used in previous builds for thumb2,
    including information that can be used for stack-unwinding.
    
    Change-Id: I8a25e9cdc242a54f0fea41c135e11312f0bbb449

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -1,9 +1,18 @@
 libc_bionic_src_files_arm += \
-    arch-arm/krait/bionic/memcpy.S \
     arch-arm/krait/bionic/memset.S \
     arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/__strcat_chk.S \
     arch-arm/krait/bionic/__strcpy_chk.S \
+
+#For some targets we don't need this optimization.
+#Corresponding flag is defined in device specific folder.
+ifeq ($(TARGET_CPU_MEMCPY_BASE_OPT_DISABLE),true)
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a15/bionic/memcpy.S
+else
+libc_bionic_src_files_arm += \
+    arch-arm/krait/bionic/memcpy.S
+endif
 
 # Use cortex-a15 versions of strcat/strcpy/strlen and standard memmove
 libc_bionic_src_files_arm += \
