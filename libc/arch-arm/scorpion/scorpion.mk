92e7c86ad4d8f4b0f301934c7546f3b7acd70be8
Nathan Chan <e334cn@gmail.com>
2013-11-19 00:39:40 -0800

    Missing memmove for scorpion arch variant
    Change-Id: Ic6a601cef42c3beb2ca661a162adde7dbaf3a3ce

--- libc/arch-arm/scorpion/scorpion.mk
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -1,7 +1,7 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/scorpion/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/scorpion/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
-$(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
+$(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/scorpion/bionic/memmove.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a15/bionic/strlen.S)
 
-#include bionic/libc/arch-arm/generic/generic.mk
+include bionic/libc/arch-arm/generic/generic.mk
