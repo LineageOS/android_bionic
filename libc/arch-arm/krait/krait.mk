f2544623ce6c2507b0fa6e8ab275f26f1547b853
Vince Leung <vincentl@codeaurora.org>
2014-01-10 18:05:46 -0800

    libc: krait: support KINGFISHER optimized memcpy
    When defined use the KINGFISHER optimized
    cortex-a9 memcpy instead to improve performance.
    
    Change-Id: Idcfb94cc1e10d8326d7a9e623baed91125d7caf6

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -1,4 +1,8 @@
-$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/krait/bionic/memcpy.S)
+ifeq ($(TARGET_USE_KINGFISHER_OPTIMIZATION),true)
+  $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a9/bionic/memcpy.S)
+else
+  $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/krait/bionic/memcpy.S)
+endif
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/krait/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
 $(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
