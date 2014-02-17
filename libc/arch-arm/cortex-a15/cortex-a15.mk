96f2d38e6bb21a4edac42635a502cdcd69f3822c
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2014-02-17 21:35:14 +0100

    Import memchr implementation from cortex-strings for Cortex A15
    Benchmarks on a Nexus 10 have shown the cortex-strings implementation
    of memchr to be 60%-70% faster (depending on input)
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
    
    Change-Id: I72b119905234570e54068375e5c3593d0647c67c

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -8,5 +8,6 @@ $(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a15/bionic/strcpy.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a15/bionic/strlen.S)
 $(call libc-add-cpu-variant-src,__STRCAT_CHK,arch-arm/cortex-a15/bionic/__strcat_chk.S)
 $(call libc-add-cpu-variant-src,__STRCPY_CHK,arch-arm/cortex-a15/bionic/__strcpy_chk.S)
+$(call libc-add-cpu-variant-src,MEMCHR,arch-arm/cortex-a15/bionic/memchr.S)
 
 include bionic/libc/arch-arm/generic/generic.mk
