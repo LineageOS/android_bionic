58fdb2799fc5e9fe5b46fb989db1f8515c1a2e3d
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-11-14 22:21:47 +0530

    linker: don't fail if executable is not position independent
    Warn but don't fail if the executable that we are dealing with is
    not position independant. A lot of device prebuilt blobs would
    fail if PIE checks are strictly enforced. (rild, gpsd, etc.)
    
    Change-Id: I2e52af04bd5d34d142d03631d3bfd7d58ebe0ca6

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -2180,7 +2180,6 @@ static ElfW(Addr) __linker_init_post_relocation(KernelArgumentBlock& args, ElfW(
     ElfW(Ehdr)* elf_hdr = reinterpret_cast<ElfW(Ehdr)*>(si->base);
     if (elf_hdr->e_type != ET_DYN) {
         __libc_format_fd(2, "error: only position independent executables (PIE) are supported.\n");
-        exit(EXIT_FAILURE);
     }
 
     // Use LD_LIBRARY_PATH and LD_PRELOAD (but only if we aren't setuid/setgid).
