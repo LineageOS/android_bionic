60a7b89100ff12ef19d7e92397cb99d43277753e
Jake Weinstein <xboxlover360@gmail.com>
2014-11-15 12:01:42 -0500

    krait: use A15 memchr
    Change-Id: I6d9c773612db02706d5a36252eb7d4458d9b6e1b

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -1,5 +1,5 @@
 libc_bionic_src_files_arm += \
-    bionic/memchr.c \
+    arch-arm/cortex-a15/bionic/memchr.S \
     arch-arm/krait/bionic/memcpy.S \
     arch-arm/krait/bionic/memset.S \
     arch-arm/krait/bionic/strcmp.S \
