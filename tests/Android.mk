59b21e1e4bbd5cdff80cca538d9c4ce31ff15e10
Elliott Hughes <enh@google.com>
2014-12-03 14:39:20 -0800

    Implement <pty.h>.
    Based on the package/apps/Terminal implementation. I'll switch them over
    shortly. This also lets us build the toybox version of netcat.
    
    Change-Id: Ia922a100141a67409264b43b937eeca07b21f344

--- tests/Android.mk
+++ tests/Android.mk
@@ -85,6 +85,7 @@ libBionicStandardTests_src_files := \
     mntent_test.cpp \
     netdb_test.cpp \
     pthread_test.cpp \
+    pty_test.cpp \
     regex_test.cpp \
     sched_test.cpp \
     search_test.cpp \
@@ -116,6 +117,7 @@ libBionicStandardTests_src_files := \
     uchar_test.cpp \
     uniqueptr_test.cpp \
     unistd_test.cpp \
+    utmp_test.cpp \
     wchar_test.cpp \
 
 libBionicStandardTests_cflags := \
@@ -291,7 +293,7 @@ bionic-unit-tests-glibc_whole_static_libraries := \
     libBionicStandardTests \
 
 bionic-unit-tests-glibc_ldlibs := \
-    -lrt -ldl \
+    -lrt -ldl -lutil \
 
 bionic-unit-tests-glibc_c_includes := \
     bionic/libc \
