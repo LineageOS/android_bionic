607f747e59a3dea20ed324419f8afc611fd66ee1
Richard Earnshaw <rearnsha@arm.com>
2014-12-08 15:21:00 +0000

    libc: ARM64: add assembly strrchr
    Change-Id: I59a0aa618bf8139dc0368af9ddf881eba5d3eadf

--- libc/arch-arm64/arm64.mk
+++ libc/arch-arm64/arm64.mk
@@ -8,7 +8,6 @@ libc_bionic_src_files_arm64 += \
     bionic/__memset_chk.cpp \
     bionic/__strcpy_chk.cpp \
     bionic/__strcat_chk.cpp \
-    bionic/strrchr.cpp \
 
 libc_freebsd_src_files_arm64 += \
     upstream-freebsd/lib/libc/string/wcscat.c \
