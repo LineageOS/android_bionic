ecfc9dfb2e82810ee7468030027c598acf0ad76b
Elliott Hughes <enh@google.com>
2014-12-15 11:06:34 -0800

    Move the generic arm memcmp.S into the generic directory.
    Change-Id: I48e4d14a0dcddbb246edbac6d0329619574ab44d

--- libc/arch-arm/scorpion/scorpion.mk
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -1,5 +1,6 @@
 # Use krait versions of memset/strcmp/memmove
 libc_bionic_src_files_arm += \
+    arch-arm/generic/bionic/memcmp.S \
     arch-arm/krait/bionic/memset.S \
     arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/memmove.S
