e49d8283fd37afc3bd24a2ec3c246408723ab95c
Anthony King <anthonydking@slimroms.net>
2014-12-28 14:28:46 +0000

    only allow R_ARM_COPY for ET_EXEC
    R_ARM_COPY is not allowed on ET_DYN. Previously a check
    wasn't done.
    
    check the e_type to see if it's ET_EXEC instead of just
    checking if it has an EXE flag
    
    Change-Id: I0471a2601a0f7280514badbc7456af7d2acafb72

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -1325,7 +1325,7 @@ static int soinfo_relocate(soinfo* si, ElfW(Rel)* rel, unsigned count, soinfo* n
             return -1;
 #ifdef ENABLE_NON_PIE_SUPPORT
             }
-            if ((si->flags & FLAG_EXE) == 0) {
+            if (reinterpret_cast<ElfW(Ehdr)*>(si->base)->e_type != ET_EXEC) {
                 /*
                  * http://infocenter.arm.com/help/topic/com.arm.doc.ihi0044d/IHI0044D_aaelf.pdf
                  *
@@ -1333,9 +1333,6 @@ static int soinfo_relocate(soinfo* si, ElfW(Rel)* rel, unsigned count, soinfo* n
                  * R_ARM_COPY may only appear in executable objects where e_type is
                  * set to ET_EXEC.
                  *
-                 * TODO: FLAG_EXE is set for both ET_DYN and ET_EXEC executables.
-                 * We should explicitly disallow ET_DYN executables from having
-                 * R_ARM_COPY relocations.
                  */
                 DL_ERR("%s R_ARM_COPY relocations only supported for ET_EXEC", si->name);
                 return -1;
