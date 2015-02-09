bcc61c8c04ff7e76cc9c64878f65ecd43cb0ad50
Manish Kumar <manishku@codeaurora.org>
2015-02-09 19:21:46 +0530

    Remove duplicate definition of stpcpy.
    Change-Id: Ie11e9e43d7179c13feed48001a83aed310fe6e3e

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -1,7 +1,6 @@
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/memcpy.S \
     arch-arm/cortex-a15/bionic/memset.S \
-    arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/__strcat_chk.S \
     arch-arm/cortex-a15/bionic/strcmp.S \
