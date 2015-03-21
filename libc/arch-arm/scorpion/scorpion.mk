fcb430b4082bdd399e1e2bf9e5a2a2cb8c5c184d
Michael Bestas <mikeioannina@gmail.com>
2015-03-21 02:47:26 +0200

    libc: scorpion: Use cortex-a15 stpcpy
    * Like krait, fixes compilation on 5.1
    
    Change-Id: I08989c0206c333f7fd0f938b6a6d6b26475d63d8

--- libc/arch-arm/scorpion/scorpion.mk
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -8,6 +8,7 @@ libc_bionic_src_files_arm += \
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/memchr.S \
     arch-arm/cortex-a15/bionic/memcpy.S \
+    arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/strcpy.S \
     arch-arm/cortex-a15/bionic/strlen.S \
