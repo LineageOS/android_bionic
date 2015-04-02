baa9f7cf7e286c1649a2ad91ba209e999bb41c0e
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-04-02 11:11:11 +0530

    cortex-a9: Add back bcopy
    Change-Id: I0e3a90d4d42e99082e1544f321e33f23d6b4998c

--- libc/arch-arm/arm.mk
+++ libc/arch-arm/arm.mk
@@ -70,19 +70,17 @@ libc_arch_dynamic_src_files_arm := arch-arm/bionic/exidx_dynamic.c
 ifeq ($(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)),)
   $(warning TARGET_$(my_2nd_arch_prefix)ARCH is arm, but TARGET_$(my_2nd_arch_prefix)CPU_VARIANT is not defined)
 endif
-ifneq ($(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)),krait)
-ifneq ($(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)),scorpion)
-ifneq ($(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)),cortex-a53)
-ifneq ($(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-ifneq ($(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)),denver)
+
+ifeq ($(filter cortex-a9 cortex-a53 denver krait scorpion, $(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT))),)
 libc_bionic_src_files_arm += \
-    upstream-openbsd/lib/libc/string/stpcpy.c \
+    upstream-openbsd/lib/libc/string/stpcpy.c
+endif
+
+ifeq ($(filter cortex-a53 denver krait scorpion, $(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT))),)
+libc_bionic_src_files_arm += \
     upstream-openbsd/lib/libc/string/bcopy.c
 endif
-endif
-endif
-endif
-endif
+
 cpu_variant_mk := $(LOCAL_PATH)/arch-arm/$(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)/$(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT).mk
 ifeq ($(wildcard $(cpu_variant_mk)),)
 $(error "TARGET_$(my_2nd_arch_prefix)CPU_VARIANT not set or set to an unknown value. Possible values are cortex-a7, cortex-a8, cortex-a9, cortex-a15, krait, scorpion, denver. Use generic for devices that do not have a CPU similar to any of the supported cpu variants.")
