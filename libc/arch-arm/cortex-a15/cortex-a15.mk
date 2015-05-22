8b8e5d32202bb15a93fa081bcaf18e1ec5dbe326
Varun Chitre <varun.chitre15@gmail.com>
2015-05-22 05:56:47 -0400

    libc : memcpy : allow to disable opt for some targets
    Change-Id: I487b9e208c032c7d01523e9b36984075027e9bff

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -1,6 +1,5 @@
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/memchr.S \
-    arch-arm/cortex-a15/bionic/memcpy.S \
     arch-arm/cortex-a15/bionic/memset.S \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/__strcat_chk.S \
@@ -9,3 +8,12 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/__strcpy_chk.S \
     arch-arm/cortex-a15/bionic/strlen.S \
     bionic/memmove.c \
+
+# Optimization not required for some targets
+ifeq ($(TARGET_CPU_MEMCPY_OPT_DISABLE),true)
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a7/bionic/memcpy.S
+else
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a15/bionic/memcpy.S
+endif
