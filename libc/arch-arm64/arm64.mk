1f4e6153abe79a7d7f1e3fa83df691fdcb0b66ab
Jake Weinstein <xboxlover360@gmail.com>
2015-08-06 17:01:47 +0000

    libc: Upstream ARM64 to newlib
    Change-Id: I9c2687a8c507070ec382859c8f409ce25db6fb43

--- libc/arch-arm64/arm64.mk
+++ libc/arch-arm64/arm64.mk
@@ -9,7 +9,6 @@ libc_bionic_src_files_arm64 += \
     bionic/__strcpy_chk.cpp \
     bionic/__strcat_chk.cpp \
     bionic/memrchr.c \
-    bionic/strrchr.cpp \
 
 libc_freebsd_src_files_arm64 += \
     upstream-freebsd/lib/libc/string/wcscat.c \
