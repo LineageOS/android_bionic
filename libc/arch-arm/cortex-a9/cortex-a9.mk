7b38aeeb2e7fe4e6df36826b95b62a3142300b40
Jake Weinstein <xboxlover360@gmail.com>
2015-09-07 16:00:34 +0000

    libc: enable assembly memmove on A9 and A15
    Change-Id: I8fbedc224d1568eb91e9f5f26fc65ef2c99dfaf2

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -10,4 +10,4 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a9/bionic/strcpy.S \
     arch-arm/cortex-a9/bionic/__strcpy_chk.S \
     arch-arm/cortex-a9/bionic/strlen.S \
-    bionic/memmove.c \
+    arch-arm/krait/bionic/memmove.S \
