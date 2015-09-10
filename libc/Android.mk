1cfb26065a6a3012bf5a0e69e0e384ea43a4b66b
Christopher R. Palmer <crpalmer@gmail.com>
2015-09-09 21:49:32 -0400

    bionic: Allow devices to add device specfic static libs
    This provides a convenient (albeit powerful enough to be dangerous)
    hook to add symbols needed to support vendor blobs that do not
    necessarily match our source code.
    
    Doing so via this hook has several advantages over patching the
    code in question:
    
    * The hacks do no pollute other repositories
    * The hacks do not have any risk of breaking any other devices
    * The hacks don't just live forever when we forget they exist
    * The hacks are all easy to find by locating them together
    
    When using this, please take extra care to include only the
    minimal code to support the change.  Keep in mind that all
    code (and all libraries your code links against) will be
    part of the address space of every single process in
    the system!
    
    Change-Id: I6dcd9ad7cee330febe1a974619144d378b67b364
    (cherry picked from commit 12eb9c556ad50585bf0067974c4db41ce2fe0784)

--- libc/Android.mk
+++ libc/Android.mk
@@ -1397,6 +1397,9 @@ LOCAL_SRC_FILES_arm += \
 LOCAL_ADDRESS_SANITIZER := false
 LOCAL_NATIVE_COVERAGE := $(bionic_coverage)
 
+# Allow devices to provide additional symbols
+LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_PROVIDES_ADDITIONAL_BIONIC_STATIC_LIBS)
+
 include $(BUILD_SHARED_LIBRARY)
 
 
