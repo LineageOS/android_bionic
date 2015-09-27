e9bad6caa9ce871f2d5c0171cf32184eb3552acd
Raja Mungamuri <Raja1612.M@gmail.com>
2015-09-27 09:44:12 +0530

    Bionic b.copy added to compile for libc for Cortex-A53 and Denver removed from condition at Arm.mk.
    This allows b.copy to be complied for 64bit processors to support
    few apps.. for this instance firefox app.
    
    Change-Id: Ib0b0e321c136c6b628243226f5e6c99210ae1b66

--- libc/arch-arm/arm.mk
+++ libc/arch-arm/arm.mk
@@ -61,7 +61,7 @@ libc_bionic_src_files_arm += \
     arch-arm/bionic/_setjmp.S \
     arch-arm/bionic/setjmp.S \
     arch-arm/bionic/sigsetjmp.S \
-    arch-arm/bionic/syscall.S \
+    arch-arm/bionic/syscall.S
 
 libc_arch_static_src_files_arm := arch-arm/bionic/exidx_static.c
 libc_arch_dynamic_src_files_arm := arch-arm/bionic/exidx_dynamic.c
@@ -76,11 +76,14 @@ libc_bionic_src_files_arm += \
     upstream-openbsd/lib/libc/string/stpcpy.c
 endif
 
-ifeq ($(filter cortex-a53 krait scorpion, $(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT))),)
+## removed cortex-a53 denver from below condition for example
+# (oneplus 2 and nexus 9 respectively)
+ifeq ($(filter krait scorpion, $(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT))),)
+ $(warning TARGET_CPU_VARIANT picked bcopy.c >> , $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT))
 libc_bionic_src_files_arm += \
     upstream-openbsd/lib/libc/string/bcopy.c
 endif
-
+ $(warning TARGET compiling following list for libc , $(libc_bionic_src_files_arm))
 cpu_variant_mk := $(LOCAL_PATH)/arch-arm/$(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)/$(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT).mk
 ifeq ($(wildcard $(cpu_variant_mk)),)
 $(error "TARGET_$(my_2nd_arch_prefix)CPU_VARIANT not set or set to an unknown value. Possible values are cortex-a7, cortex-a8, cortex-a9, cortex-a15, krait, scorpion, denver. Use generic for devices that do not have a CPU similar to any of the supported cpu variants.")
