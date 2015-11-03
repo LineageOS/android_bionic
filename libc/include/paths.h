e2f0539dee416d6770493a0d90e1bdffa874acf1
Tom Marshall <tdm@cyngn.com>
2015-11-03 11:12:23 -0800

    bionic: Let popen fall back to /sbin/sh
    minivold in recovery uses popen, where /system/bin/sh is not available.
    
    Change-Id: I2136b0ca4188b7b44416f5d79492fc006382d4ad

--- libc/include/paths.h
+++ libc/include/paths.h
@@ -33,6 +33,7 @@
 #define	_PATHS_H_
 
 #define	_PATH_BSHELL	"/system/bin/sh"
+#define	_PATH_BSHELL2	"/sbin/sh"
 #define	_PATH_CONSOLE	"/dev/console"
 #define	_PATH_DEFPATH	"/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin"
 #define	_PATH_DEV	"/dev/"
