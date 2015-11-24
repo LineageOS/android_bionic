f3d29eef755b141a5b51c45d177d69d823290b4b
Steve Kondik <steve@cyngn.com>
2015-11-24 14:11:53 -0800

    libc: Add memchr to a53+a57 target
    Change-Id: I3991c06dd4813c7cc73d50b56fd7974116d79892

--- libc/arch-arm/cortex-a53.a57/cortex-a53.a57.mk
+++ libc/arch-arm/cortex-a53.a57/cortex-a53.a57.mk
@@ -16,6 +16,7 @@ libc_bionic_src_files_arm += \
 
 libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/memcmp.S \
+    arch-arm/generic/bionic/memchr.S
 
 libc_bionic_src_files_arm += \
     arch-arm/denver/bionic/memmove.S \
