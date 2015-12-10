ed7a92c4ce10cad2ac583469f01971f55765bfa1
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-12-10 09:16:55 -0800

    Denver64: don't use arm64 optimized memmove
    Change-Id: I0ae1d3857ef6834cc76429bee2f8d2fd5defdea9

--- libc/arch-arm64/denver64/denver64.mk
+++ libc/arch-arm64/denver64/denver64.mk
@@ -2,7 +2,7 @@ libc_bionic_src_files_arm64 += \
     arch-arm64/generic/bionic/memchr.S \
     arch-arm64/generic/bionic/memcmp.S \
     arch-arm64/denver64/bionic/memcpy.S \
-    arch-arm64/generic/bionic/memmove.S \
+    arch-arm64/denver64/bionic/memmove.S \
     arch-arm64/denver64/bionic/memset.S \
     arch-arm64/generic/bionic/stpcpy.S \
     arch-arm64/generic/bionic/strchr.S \
