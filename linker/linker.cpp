27f93f0cf44e1b8aa1d8941cb09c1eec2d201372
Adrian DC <radian.dc@gmail.com>
2016-08-27 15:09:36 +0200

    linker: Make text relocations denial optional
     * Use the TARGET_NEEDS_TEXT_RELOCATIONS := true
        configuration to allow a device to use
        legacy proprietary libraries like camera
    
    Change-Id: I994ab1a600a0b237b496ceebe2dd54febc28a6bd

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -4004,12 +4004,14 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 #if !defined(__LP64__)
   if (has_text_relocations) {
     // Fail if app is targeting sdk version > 22
+#if !defined(TARGET_NEEDS_TEXT_RELOCATIONS)
 #if !defined(__i386__) // ffmpeg says that they require text relocations on x86
     if (get_application_target_sdk_version() > 22) {
       PRINT("%s: has text relocations", get_realpath());
       DL_ERR("%s: has text relocations", get_realpath());
       return false;
     }
+#endif
 #endif
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied.
