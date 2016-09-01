610f41c28a4106f21f125a27fc323bcc9c20f321
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-01 09:20:26 +0200

    libc: Always included generic arm64 routines
    Change-Id: I77d36626968f205ee8dc1531390dd477d620683d
    (cherry picked from commit 89cf69de74b569ffa17c41461c080d5f53027e68)

--- libc/arch-arm64/arm64.mk
+++ libc/arch-arm64/arm64.mk
@@ -3,7 +3,6 @@
 #
 # Generic arm64 optimizations, may be overriden by CPU variants.
 #
-ifeq ($(TARGET_CPU_VARIANT),generic)
 libc_bionic_src_files_arm64 += \
     arch-arm64/generic/bionic/memchr.S \
     arch-arm64/generic/bionic/memcmp.S \
@@ -18,8 +17,6 @@ libc_bionic_src_files_arm64 += \
     arch-arm64/generic/bionic/strncmp.S \
     arch-arm64/generic/bionic/strnlen.S \
     arch-arm64/generic/bionic/wmemmove.S \
-
-endif
 
 libc_bionic_src_files_exclude_arm64 += \
     bionic/__memcpy_chk.cpp \
