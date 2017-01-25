91b06f97680cfceabba61c1b7859de18d1709312
Matt Mower <mowerm@gmail.com>
2017-01-25 11:39:38 -0600

    linker: Allow only specified non-PIE executables
    For devices that rely on (hopefully a small number of) vendor provided
    non position independent executables, a listing of those executables
    can be provided to the linker to allow them to run. This limits the
    severity of bypassing this security feature entirely.
    
    Header file linker_non_pie_executables.h should be created per-device,
    where the syntax of linker_non_pie_executables[] can be referenced
    from linker_non_pie.cpp in this commit. The linker is made aware of
    the header's existence via:
    
      LINKER_NON_PIE_EXECUTABLES_HEADER_DIR := path/to/include
    
    This commit is designed very closely to commit 'non-pie: Allow only
    select libs to be non-pie' (Ife1b2fcc75b418379fc382ae276e63905e8528e0)
    by Anthony King, but has been amended for use with Android 7.1.
    
    Change-Id: If4fe98e64bbdd91bfdec5de83247ea3d5688437e

--- linker/Android.mk
+++ linker/Android.mk
@@ -61,8 +61,10 @@ endif
 # We need to access Bionic private headers in the linker.
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/../libc/
 
-ifeq ($(TARGET_NEEDS_NON_PIE_SUPPORT),true)
-  LOCAL_CFLAGS += -DENABLE_NON_PIE_SUPPORT
+ifneq ($(LINKER_NON_PIE_EXECUTABLES_HEADER_DIR),)
+    LOCAL_CFLAGS += -DENABLE_NON_PIE_SUPPORT
+    LOCAL_C_INCLUDES += $(LINKER_NON_PIE_EXECUTABLES_HEADER_DIR)
+    LOCAL_SRC_FILES += linker_non_pie.cpp
 endif
 
 # we don't want crtbegin.o (because we have begin.o), so unset it
