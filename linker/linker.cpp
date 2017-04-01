a955890a4bd9ee435fb2b462ec951937d53f814e
sayeed99 <sayeed.afridi2009@gmail.com>
2017-04-01 09:26:15 +0530

    Workaround for CAF based x86 ROMs
    Change-Id: I7a8b1af6ac1a5360feb59d0bffdcb0b4fb01fbc3

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -4059,23 +4059,8 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 #if !defined(__LP64__)
   if (has_text_relocations) {
     // Fail if app is targeting sdk version > 22
-#if !defined(__i386__) // ffmpeg says that they require text relocations on x86
-#if defined(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)
-    if (get_application_target_sdk_version() != __ANDROID_API__
-        && get_application_target_sdk_version() > 22) {
-#else
-    if (get_application_target_sdk_version() > 22) {
-#endif
-      PRINT("%s: has text relocations", get_realpath());
-      DL_ERR("%s: has text relocations", get_realpath());
-      return false;
-    }
-#endif
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied.
-    DL_WARN("%s has text relocations. This is wasting memory and prevents "
-            "security hardening. Please fix.", get_realpath());
-    add_dlwarning(get_realpath(), "text relocations");
     if (phdr_table_unprotect_segments(phdr, phnum, load_bias) < 0) {
       DL_ERR("can't unprotect loadable segments for \"%s\": %s",
              get_realpath(), strerror(errno));
