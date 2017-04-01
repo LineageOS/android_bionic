15ab01670abc165c0388f42222c7189668b625c1
sayeed99 <sayeed.afridi2009@gmail.com>
2017-04-01 11:15:14 +0530

    Linker : Properly allow text relocations. It doesn't makes sense to warn user on the UI that ffmpeg libs have text relocations. FFMPEG is not going to we write the whole code. Since x86 already allows text reloactions, just don't print any warning against it.
    Change-Id: I107bb59bb5c922faf95c33aaaec4e8d4f1f781f9

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -4070,12 +4070,12 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
       DL_ERR("%s: has text relocations", get_realpath());
       return false;
     }
-#endif
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied.
     DL_WARN("%s has text relocations. This is wasting memory and prevents "
             "security hardening. Please fix.", get_realpath());
     add_dlwarning(get_realpath(), "text relocations");
+#endif
     if (phdr_table_unprotect_segments(phdr, phnum, load_bias) < 0) {
       DL_ERR("can't unprotect loadable segments for \"%s\": %s",
              get_realpath(), strerror(errno));
