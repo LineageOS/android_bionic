147baa7bc04bd5f2011780deac88a24cfa58e197
Nich <nctrenco@gmail.com>
2018-06-15 03:59:05 +0800

    linker: Ensure active matching pairs
    Change-Id: I54c666b4560dbfb40839b0bf9132a7fd8d3ed2dd

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -710,10 +710,6 @@ static std::vector<ShimDescriptor> g_ld_all_shim_libs;
 // for libB where libA also links against libB).
 static linked_list_t<const ShimDescriptor> g_active_shim_libs;
 
-// matched_pairs are shim libs that load over their corresponding
-// target libraries/executables, which are DT_NEEDED.
-std::vector<const ShimDescriptor *> matched_pairs;
-
 static void reset_g_active_shim_libs(void) {
   g_active_shim_libs.clear();
   for (const auto& pair : g_ld_all_shim_libs) {
@@ -738,18 +734,20 @@ void parse_LD_SHIM_LIBS(const char* path) {
   reset_g_active_shim_libs();
 }
 
-void shim_matching_pairs(const char *const path) {
-  INFO("Finding shim libs for \"%s\"\n", path);
+std::vector<const ShimDescriptor*> shim_matching_pairs(const char* path) {
+  std::vector<const ShimDescriptor*> matched_pairs;
 
-  g_active_shim_libs.for_each([&](const ShimDescriptor *a_pair) {
+  g_active_shim_libs.for_each([&](const ShimDescriptor* a_pair) {
     if (a_pair->first == path) {
       matched_pairs.push_back(a_pair);
     }
   });
 
-  g_active_shim_libs.remove_if([&](const ShimDescriptor *a_pair) {
+  g_active_shim_libs.remove_if([&](const ShimDescriptor* a_pair) {
     return a_pair->first == path;
   });
+
+  return matched_pairs;
 }
 #endif
 
